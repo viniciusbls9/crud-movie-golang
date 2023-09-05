b4084e36734ef05b2b97d00d05d317ffd40e1ca0
viniciusbls9 <beneditoloura@gmail.com>
2023-09-05 07:53:09 -0300

    feat: create test to respondWithJSON func

--- handler_responseJSON_test.go
+++ handler_responseJSON_test.go
@@ -34,3 +34,38 @@ func TestRespondWithError(t *testing.T) {
 func parseJSON(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
+
+func TestRespondWithJSON(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	payload := struct {
+		Message string `json:"message"`
+	}{
+		Message: "Hello, World!",
+	}
+
+	respondWithJSON(w, http.StatusOK, payload)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
+	}
+
+	contentType := w.Header().Get("Content-Type")
+	expectedContentType := "application/json"
+	if contentType != expectedContentType {
+		t.Errorf("Expected Content-Type %s, but got %s", expectedContentType, contentType)
+	}
+
+	var response struct {
+		Message string `json:"message"`
+	}
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("Failed to parse JSON response: %v", err)
+	}
+
+	expectedMessage := "Hello, World!"
+	if response.Message != expectedMessage {
+		t.Errorf("Expected message '%s', but got '%s'", expectedMessage, response.Message)
+	}
+}
